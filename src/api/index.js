6afdee638cc4c18f6614fb8ed0f7912ddcec380c
Siva Karthik <siramesh@akamai.com>
2020-05-03 16:44:30 +0530

    Updating API to provide country wise data

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let modifiedURL = url;
+    if(country)
+    {
+        modifiedURL = `${url}/countries/${country}`;
+    }
+
     try{
-        const { data: {confirmed,recovered,deaths,lastUpdate} } = await axios.get(url);
+        const { data: {confirmed,recovered,deaths,lastUpdate} } = await axios.get(modifiedURL);
         return { confirmed,recovered,deaths, lastUpdate };
     }
     catch(error)
@@ -30,10 +36,10 @@ export const fetchDailyData = async () => {
     }
 }
 
-export const countries = async () => {
+export const fetchCountries = async () => {
     try{
-        const response = await axios.get(`${url}/countries`);
-        console.log(response);
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name);
 
     }
     catch(error)
